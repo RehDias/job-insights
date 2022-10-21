862325ca1ecfef4f8ef80ae16672c290097fa3cf
Renata Dias <renata_dias96@live.com>
2022-10-21 15:37:31 -0300

    test: Implementação dos testes das funções count_ocurrences e read_brazilian_file

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,5 +1,8 @@
-# from src.brazilian_jobs import read_brazilian_file
+from src.brazilian_jobs import read_brazilian_file
 
 
 def test_brazilian_jobs():
-    pass
+    jobs = read_brazilian_file('tests/mocks/brazilians_jobs.csv')
+    for job in jobs:
+        assert job.keys() == {'title', 'salary', 'type'}
+        assert job.keys() is not {'titulo', 'salario', 'tipo'}
--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,8 @@
-# from src.counter import count_ocurrences
+from src.counter import count_ocurrences
 
 
 def test_counter():
-    pass
+    count_developer = count_ocurrences('tests/mocks/jobs.csv', 'Developer')
+    count_full_time = count_ocurrences('tests/mocks/jobs.csv', 'full Time')
+    assert count_developer == 3
+    assert count_full_time == 2
